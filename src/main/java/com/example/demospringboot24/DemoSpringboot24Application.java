5e9bee78bede5fe6dd2e46dbe12295a53fb8e4a5
jinwoo park <swat018@gmail.com>
2022-01-21 06:42:43 +0900

    Initial commit

---
+++ src/main/java/com/example/demospringboot24/DemoSpringboot24Application.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringboot24Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demospringboot24/DemoSpringboot24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringboot24ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
