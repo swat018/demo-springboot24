76947750918ad31a82e1f020d62c66d06235e90e
jinwoo park <swat018@gmail.com>
2022-01-22 10:35:19 +0900

    feat : 설정 파일 처리 방식 변경

--- src/main/java/com/example/demospringboot24/DemoSpringboot24Application.java
+++ src/main/java/com/example/demospringboot24/DemoSpringboot24Application.java
@@ -1,13 +1,34 @@
 package com.example.demospringboot24;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoSpringboot24Application {
 
+    @Value("${my.message}")
+    private String myMessage;
+
+    @Autowired
+    private Environment environment;
+
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpringboot24Application.class, args);
+        SpringApplication app = new SpringApplication(DemoSpringboot24Application.class);
+        app.setWebApplicationType((WebApplicationType.NONE));
+        app.run(args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> System.out.println(myMessage + " " + Arrays.toString(environment.getActiveProfiles()));
     }
 
 }
